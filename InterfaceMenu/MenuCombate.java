5bb303b096d888a192e43c16320ffd0178419e9e
ana soares <alocinnydev@gmail.com>
2024-06-02 19:48:54 -0300

    adicionando o get interface gráfica

--- InterfaceMenu/MenuCombate.java
+++ InterfaceMenu/MenuCombate.java
@@ -9,12 +9,16 @@ import javax.swing.border.*;
 public class MenuCombate extends JFrame{
 
     private JPanel contentPanel;
+
+    private int escolha;
+    private int atacar = 1;
+    private int defender = 2;
     
     public void menuCombate(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100,100,450,300);
         contentPanel = new JPanel();
-        contentPanel.setBorder(new EmptyBorder(5,5,5,5));
+        contentPanel.setBorder(new EmptyBorder(10,10,10,10));
         
         JFrame Win = new JFrame("teste");
         Win.setSize(1600, 900);
@@ -27,19 +31,32 @@ public class MenuCombate extends JFrame{
         JButton botaoAtacar = new JButton("ATACAR");
         botaoAtacar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent k){
+                escolhaMenu(atacar);
+                getEscolhaMenu();
                 JOptionPane.showMessageDialog(null, "voce atacou");
             }
         });
         botaoAtacar.setBounds(300,300,100,30);
         Win.add(botaoAtacar);
 
+        //botão de defender
         JButton botaoDefender = new JButton("DEFENDER");
         botaoDefender.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent k){
+                escolhaMenu(defender);
+                getEscolhaMenu();
                 JOptionPane.showMessageDialog(null, "voce defendeu");
             }
         });
         botaoDefender.setBounds(300,350,100,30);
         Win.add(botaoDefender);
+    }
+
+    public void escolhaMenu(int escolhaId){
+        this.escolha = escolhaId;
+    }
+
+    public int getEscolhaMenu(){
+        return this.escolha;
     }
 }
