7476d7aa55575224f8c757793e804ea2c723a1a2
ana soares <alocinnydev@gmail.com>
2024-06-02 19:22:15 -0300

    interface gráfica menu combate

---
+++ InterfaceMenu/MenuCombate.java
@@ -0,0 +1,37 @@
+package InterfaceMenu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class MenuCombate extends JFrame{
+
+    private JPanel contentPanel;
+    
+    public void menuCombate(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100,100,450,300);
+        contentPanel = new JPanel();
+        contentPanel.setBorder(new EmptyBorder(5,5,5,5));
+        
+        JFrame Win = new JFrame("teste");
+        Win.setSize(1600, 900);
+        Win.setVisible(true);
+
+        setContentPane(contentPanel);
+        contentPanel.setLayout(null);
+
+        //botão de atacar
+        JButton botaoAtacar = new JButton("ATACAR");
+        botaoAtacar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent k){
+                JOptionPane.showMessageDialog(null, "voce atacou");
+            }
+        });
+        botaoAtacar.setBounds(400,300,90,25);
+        Win.add(botaoAtacar);
+
+    }
+}
