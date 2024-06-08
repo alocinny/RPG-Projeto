fe5e626cf6a3f1aad6fc373eea54f72200bc8854
ana soares <alocinnydev@gmail.com>
2024-06-08 11:40:01 -0300

    update interface da progressao de nivel

--- Menu/MenuProgressaoNivel.java
+++ Menu/MenuProgressaoNivel.java
@@ -1,5 +1,95 @@
 package Menu;
 
-public class MenuProgressaoNivel {
-    
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import Agentes.Agentes;
+import SistemaRPG.ProgressaoNivel;
+
+public class MenuProgressaoNivel extends JFrame {
+
+    private int[] sizeWin = {600, 600};
+    private String mensagem = "Sem xp suficiente";
+    private JFrame frame;
+    private JPanel jPanel;
+    private JLabel jLabel;
+    private JLabel jLabelAfinidade;
+    private JLabel jLabelAgilidade;
+    private JLabel jLabelXp;
+    private JButton jButtonAfinidade;
+    private JButton jButtonAgilidade;
+
+    public void winMenuProgressaoNivel(ProgressaoNivel progressaoNivel, Agentes agentes){
+
+        frame = new JFrame("Progressão de nível");
+        jLabel = new JLabel();
+        jPanel = new JPanel();
+        jButtonAfinidade = new JButton("Progredir afinidade");
+        jButtonAgilidade = new JButton("Progredir agilidade");
+
+        //mostra na janela a agilidade total do agente
+        jLabelAgilidade = new JLabel("Agilidade: " + agentes.getAgilidade());
+        jLabelAgilidade.setBounds(50,50,200,30);
+
+        //mostra na janela o vigor total do agente
+        jLabelAfinidade = new JLabel("Afinidade: " + agentes.getAfinidade());
+        jLabelAfinidade.setBounds(50,50,200,30);
+
+        //mostra na janela o xp total do agente
+        jLabelXp = new JLabel("Xp: " + progressaoNivel.getXp());
+        jLabelXp.setBounds(50,50,200,30);
+
+        //botao para progredir Afinidade
+        jButtonAfinidade.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                if(progressaoNivel.getXp()<3){
+                    JOptionPane.showMessageDialog(null, mensagem);
+                } else {
+                    progressaoNivel.newXpDecrementaAfinidade();
+                    jLabelAfinidade.setText("Afinidade: " + agentes.getAfinidade());
+                    jLabelXp.setText("Xp: " + progressaoNivel.getXp());
+                }
+            }
+        });
+        jButtonAfinidade.setBounds(20,40,250,40);
+
+        //botao para progredir Agilidade
+        jButtonAgilidade.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                if(progressaoNivel.getXp()<2){
+                    JOptionPane.showMessageDialog(null, mensagem);
+                } else {
+                    progressaoNivel.newXpDecrementaAgilidade();
+                    jLabelAgilidade.setText("Agilidade: " + agentes.getAgilidade());
+                    jLabelXp.setText("Xp: " + progressaoNivel.getXp());
+                }
+            }
+        });
+        jButtonAgilidade.setBounds(20,90,250,40);
+
+        //atualiza a interface
+        
+        //adiciona os labels no panel
+        jPanel.add(jLabel);
+        jPanel.add(jLabelAfinidade);
+        jPanel.add(jLabelAgilidade);
+        jPanel.add(jLabelXp);
+        
+        //adiciona botoes no panel
+        frame.add(jButtonAfinidade);
+        frame.add(jButtonAgilidade);
+        
+        //confugirações da janela
+        frame.add(jPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(600, 200, sizeWin[0], sizeWin[1]);
+        frame.setVisible(true);
+        frame.setLayout(null);
+    }
 }
