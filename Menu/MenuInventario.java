a5988286a5b9dc20026dae6c02296427b8750a48
ana soares <alocinnydev@gmail.com>
2024-07-01 00:11:03 -0300

    adição de botao para dropar itens - bug

--- Menu/MenuInventario.java
+++ Menu/MenuInventario.java
@@ -1,12 +1,14 @@
 package Menu;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import Agentes.Agentes;
-import SistemaRPG.Inventario;
-
 public class MenuInventario extends JFrame{
 
     private int[] sizeWin = {600, 600};
@@ -14,23 +16,34 @@ public class MenuInventario extends JFrame{
     private JPanel jPanel;
     private JLabel jLabel;
     private JLabel jLabelItens;
+    private JButton jButtonRemoverItem;
 
-    public void winMenuInventario(Inventario inventario, Agentes agente){
+    public void winMenuInventario(Agentes agente){
 
         frame = new JFrame("Inventário");
         jLabel = new JLabel();
         jPanel = new JPanel();
 
-        jLabelItens = new JLabel(" " + agente.getInventario());
+        jLabelItens = new JLabel(agente.getInventario());
         jLabelItens.setBounds(50,50,200,30);
+
+        jButtonRemoverItem = new JButton("remover item");
+        jButtonRemoverItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                
+                jLabel.setText(agente.getInventario());
+            }
+        });
+        jButtonRemoverItem.setBounds(20,40,250,40);
 
         jPanel.add(jLabel);
         jPanel.add(jLabelItens);
+        frame.add(jButtonRemoverItem);
 
         frame.add(jPanel);
+        frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         frame.setBounds(600, 200, sizeWin[0], sizeWin[1]);
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         frame.setLayout(null);
     }
 }
