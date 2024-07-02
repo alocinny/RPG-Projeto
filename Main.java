c711fbe2312a749411a8852e4f07d886d56fb908
Biazita14 <beatriz.bbbferraz@gmail.com>
2024-07-01 23:14:19 -0300

    adicionando exceção na main

--- Main.java
+++ Main.java
@@ -9,88 +9,89 @@ import Agentes.OcultistaSangue;
 import Mapa.Mapa;
 import Menu.MenuJogo;
 import SistemaRPG.ProgressaoNivel;
-import SistemaRPG.SistemaDialogo;
 
 public class Main {
 
     private static int escolha;
     private static Agentes agente;
 
-    
     public static void main(String[] args) {
 
-        int[] habilidadeSangue = {3,4,6,5,5};
-        int[] saudeSangue = {110,0};
-    
-        int[] habilidadeMedo = {4,3,6,5,5};
-        int[] saudeMedo = {100,0};
-    
-        int[] habilidadeConhecimento = {5,2,8,4,5};
-        int[] saudeConhecimento = {90,0};
+        int[] habilidadeSangue = {3, 4, 6, 5, 5};
+        int[] saudeSangue = {110, 0};
 
-        //andar com w,a,s,d, pressionar 'e' para acessar o menul e 'p' para sair
+        int[] habilidadeMedo = {4, 3, 6, 5, 5};
+        int[] saudeMedo = {100, 0};
 
-        //mapa
-        Mapa mapa = new Mapa(50,30);
+        int[] habilidadeConhecimento = {5, 2, 8, 4, 5};
+        int[] saudeConhecimento = {90, 0};
 
-        //agentes
+        // andar com w,a,s,d, pressionar 'e' para acessar o menul e 'p' para sair
+
+        // mapa
+        Mapa mapa = new Mapa(50, 30);
+
+        // agentes
         OcultistaConhecimento ocultistaConhecimento = new OcultistaConhecimento(habilidadeConhecimento, saudeConhecimento);
         OcultistaMedo ocultistaMedo = new OcultistaMedo(habilidadeMedo, saudeMedo);
         OcultistaSangue ocultistaSangue = new OcultistaSangue(habilidadeSangue, saudeSangue);
         MenuJogo menuJogo = new MenuJogo();
 
-        SistemaDialogo sistemaDialogo = new SistemaDialogo();
-        sistemaDialogo.lerArquivoEDialogar("SistemaRPG\\dialogoVerissimo.txt");
-
         Scanner scanner = new Scanner(System.in);
-        
-        //menu temporário (deepois vamos fazer GUI)
-        
+
+        // menu temporário (depois vamos fazer GUI)
+
         System.out.println("ESCOLHA UM AGENTE");
         System.out.println("1 - Ocultista Conhecimento:\nagilidade: " + ocultistaConhecimento.getAgilidade() + "\nforça: " + ocultistaConhecimento.getForca() + "\nvigor: " + ocultistaConhecimento.getvigor() + "\natletismo: " + ocultistaConhecimento.getAtletismo() + "\n");
         System.out.println("2 - Ocultista Medo:\nagilidade: " + ocultistaMedo.getAgilidade() + "\nforça: " + ocultistaMedo.getForca() + "\nvigor: " + ocultistaMedo.getvigor() + "\natletismo: " + ocultistaMedo.getAtletismo() + "\n");
         System.out.println("3 - Ocultista Sangue:\nagilidade: " + ocultistaSangue.getAgilidade() + "\nforça: " + ocultistaSangue.getForca() + "\nvigor: " + ocultistaSangue.getvigor() + "\natletismo: " + ocultistaSangue.getAtletismo() + "\n");
-        
-        escolha = scanner.nextInt();
-        
-        switch (escolha) {
-            case 1:
-            agente = new Agentes(ocultistaConhecimento.getHabilidades(), ocultistaConhecimento.getSaude());
-            break;
-            case 2: 
-            agente = new Agentes(ocultistaMedo.getHabilidades(), ocultistaMedo.getSaude());
-            break;
-            case 3: 
-            agente = new Agentes(ocultistaSangue.getHabilidades(), ocultistaSangue.getSaude());
-            break;
-            default:
-            break;
-        }
-        
-        ProgressaoNivel progressaoNivel = new ProgressaoNivel(agente.getPtOcultismo(),agente);
 
-        System.out.println();
-        
-        mapa.setAgente(agente);
-        
-        while(agente.vivo()){
+        try {
+            escolha = scanner.nextInt();
 
-            mapa.drawMapa();
-        
-            char direction = scanner.next().charAt(0);
-
-            if(direction == 'p'){
-                scanner.close();
-                break;
-            } 
-
-            if(direction == 'e'){
-                menuJogo.winMenuJogo(progressaoNivel, agente);
+            switch (escolha) {
+                case 1:
+                    agente = new Agentes(ocultistaConhecimento.getHabilidades(), ocultistaConhecimento.getSaude());
+                    break;
+                case 2:
+                    agente = new Agentes(ocultistaMedo.getHabilidades(), ocultistaMedo.getSaude());
+                    break;
+                case 3:
+                    agente = new Agentes(ocultistaSangue.getHabilidades(), ocultistaSangue.getSaude());
+                    break;
+                default:
+                    throw new IllegalArgumentException("Escolha inválida. Por favor, escolha 1, 2 ou 3.");
             }
 
-            agente.move(direction,mapa,agente);
-        }
+            ProgressaoNivel progressaoNivel = new ProgressaoNivel(agente.getPtOcultismo(), agente);
 
-        System.out.println("fim");
+            System.out.println();
+
+            mapa.setAgente(agente);
+
+            while (agente.vivo()) {
+
+                mapa.drawMapa();
+
+                char direction = scanner.next().charAt(0);
+
+                if (direction == 'p') {
+                    scanner.close();
+                    break;
+                }
+
+                if (direction == 'e') {
+                    menuJogo.winMenuJogo(progressaoNivel, agente);
+                }
+
+                agente.move(direction, mapa, agente);
+            }
+
+            System.out.println("fim");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
+        }
     }
 }
